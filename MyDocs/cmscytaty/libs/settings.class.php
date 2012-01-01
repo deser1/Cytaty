4a2570f8415825bfdab174ec24f4ac951d61ed48
deser1 <deser1@o2.pl>
2012-01-01 01:23:40 +0100

    Start projektu

---
+++ MyDocs/cmscytaty/libs/settings.class.php
@@ -0,0 +1,37 @@
+<?php
+class Settings{
+	private $config;
+	protected function __construct(){
+		$this->config = array();
+	}
+	public function Add($attributes, $value = ''){
+		if (!is_array($this->config)){
+			$this->config = $this->config[$attributes][$value];
+		}else{
+			$this->config = array_merge($this->config, $attributes);
+		}
+	}
+	public function Value($val = ''){
+		if($val === ''){
+			return $this->config;
+		}elseif ($val !== ''){
+			return $this->config[$val];
+		}
+	}
+	public function XML(){
+		foreach($this->config as $key=>$value){
+			$this->xml .= '<'.$key.'>';
+			if (is_array($value)){
+				foreach($value as $key2=>$value2){
+					$this->xml .= '<'.$key2.'>';
+					$this->xml .= '<'.$value2.'/>';
+					$this->xml .= '</'.$key2.'>';
+				}
+			}else{
+				$this->xml .= '<'.$value.'/>';
+			}
+			$this->xml .= '</'.$key.'>';
+		}
+	}
+}
+?>
---
+++ MyDocs/cmscytaty/libs/user.class.php
@@ -0,0 +1,6 @@
+<?php
+  class User{
+    private $id;
+    private $db;
+  }
+?>
